4f98abbfd7d50fe9126a35f2875be50174053807
Chandrashekhar-D <umailtochr@gmail.com>
2021-10-29 21:19:17 +0530

    Added Fibonacci function

--- math.cpp
+++
@@ -1,5 +0,0 @@
-int add(int num1, int num2) {return num1 + num2;}
-int subtract(int num1, int num2) {return num1 - num2;}
-int multiply(int num1, int num2) {return num1 * num2;}
-int divide(int num1, int num2) {return num1/num2;}
-int square(int num) {  return num * num;}
--- script.js
+++ script.js
@@ -1,19 +1,48 @@
-let math;
 
+// Let's create a function called loadWebAssembly that converts given file into binary array buffer.
 function loadWebAssembly(fileName) {
   return fetch(fileName)
     .then(response => response.arrayBuffer())
     .then(buffer => WebAssembly.compile(buffer))
     .then(module => {return new WebAssembly.Instance(module) });
-};
-  
+};   
+
+//We call the function for math.wasm
 loadWebAssembly('math.wasm')
   .then(instance => {
-    add = instance.exports._Z3addii;
-    subtract = instance.exports._Z8subtractii;
-    multiply = instance.exports._Z8multiplyii;
-    divide = instance.exports._Z6divideii;
-    square = instance.exports._Z6squarei;
+  fibc = instance.exports._Z3fibi;
 
-    console.log('Call your math functions !');
+    console.log('Call your functions !');
   }); 
+
+// Function written in Javascript for nth fibonacci 
+  function fibj(n)
+{
+   if (n <= 1)
+      return n;
+   return fibj(n-1) + fibj(n-2);
+}
+  
+//This function gives the time required for C++ function
+function perfoc(n){
+var startTime = performance.now()
+
+var c=fibc(n)
+    
+var endTime = performance.now()
+
+console.log(`Calculating nth Fibonacci with WASM took ${endTime - startTime} milliseconds,nth fibonacci is ${c}`)
+
+}
+
+// This function gives the time required for Javascript function
+function perfoj(n){
+  var startTime = performance.now()
+  
+  var j=fibj(n)
+      
+  var endTime = performance.now()
+  
+  console.log(`Calculating nth Fibonacci with JS took ${endTime - startTime} milliseconds, nth fibonacci is ${j}`)
+  
+  }
